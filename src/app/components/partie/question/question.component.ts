f54cd9bb44c7c7447466d3e1230cff823f103c88
arnaudetitia <arnaud.etitia@gmail.com>
2026-09-11 22:58:33 +0200

    refacto affichage question

--- src/app/components/partie/question/question.component.ts
+++ src/app/components/partie/question/question.component.ts
@@ -61,10 +61,6 @@ export class QuestionComponent {
   }
 
   revealQuestion() {
-    this.startTransition = true;
-    setTimeout(() => {
-      this.questionShown.set(true);
-      this.chronoOrchestrator.allowChronoToStart();
-    }, 750);
+    this.questionShown.set(true);
   }
 }
